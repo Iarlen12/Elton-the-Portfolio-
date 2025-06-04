ab33477866d1a7ddcc7b9c07c63dc31037837c38
Elton <166235259+Iarlen12@users.noreply.github.com>
2025-06-04 19:41:56 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 function Menu(){
   const menuLateral = document.querySelector('.menu-lateral')
   const menuIcon = document.querySelector('.menu-mobile')
-    menuLateral.classList.add('active')
     
-    if (menuIcon) {
+    if ( menuIcon) {
         menuIcon.addEventListener('click', () => {
           menuLateral.classList.toggle('active')
         })  
     }
    
 }
+window.addEventListener('DOMContentLoaded', Menu);
